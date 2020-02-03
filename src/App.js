b25f346bf932d6e62eb2ce38151359388ecb746f
Rashmi <rashmi.manandhar7@gmail.com>
2020-02-03 16:39:00 -0500

    feat: Get books from api
    * show the books in their corresponding category
    * create a bookshelf and book component
    * show the current shelf as selected

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import SearchBooks from "./searchBooks.js"
-import ListBooks from "./listBooks.js"
+import BookShelf from "./BookShelf.js"
 class BooksApp extends React.Component {
-	render() {
-		return (<div className="app">
-    <Route exact path="/" render={()=>(<ListBooks/>)} />
-    <Route path="/search" render={({history}) => (<SearchBooks/>)}/>
-
-      </div>)
+	state = {
+		books: []
 	}
+	componentDidMount() {
+		BooksAPI.getAll()
+			.then((booksFromAPI) => {
+				this.setState((prevState) => ({
+					books: booksFromAPI
+				}))
+			});
+	}
+	render() {
+		console.log(this.state.books);
+		const { books } = this.state;
+		const readBooks = books.filter((book) => (book.shelf === 'read'));
+		const currentlyReadingBooks = books.filter((book) => (book.shelf === 'currentlyReading'));
+		const wantToReadBooks = books.filter((book) => (book.shelf === 'wantToRead'));
+		console.log(readBooks);
+		return (<div className="app">
+    <Route exact path="/" render={()=>(
+      <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+              <BookShelf name="Currently Reading" books={currentlyReadingBooks}/>
+              <BookShelf name="Want to Read" books={wantToReadBooks}/>
+              <BookShelf name="Read" books={readBooks}/>
+            </div> < /div> < div className = "open-search" > <Link to='/search
+			' className="add-contact">
+			Add a book < /Link> < /div > < /div>)
+	}
+	/> < Route path = "/search"
+	render = {
+		({ history }) => (<SearchBooks/>)
+	}
+	/> < /div > )
 }
-export default BooksApp;
+}
+export default BooksApp
---
+++ src/Book.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+function Book(props){
+  const options = [{
+    value: 'currentlyReading',
+    text: 'Currently Reading'
+  },
+  {
+    value: 'wantToRead',
+    text: 'Want to Read'
+  },
+  {
+    value: 'read',
+    text: 'Read'
+  },
+  {
+    value: 'none',
+    text: 'None'
+  }]
+  const {book} = props;
+  return(
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+          <div className="book-shelf-changer">
+            <select value= {book.shelf}>
+            <option value="move" disabled>Move to...</option>
+            {options.map((option)=>(
+              <option key={option.value} value={option.value}>
+              {option.text}</option>
+
+            ))}
+
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">
+        {book.authors.join(', ')}
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default Book;
