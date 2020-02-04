7960140786e76751f7be726c9c5fcb37755687bd
Rashmi <rashmi.manandhar7@gmail.com>
2020-02-04 12:55:45 -0500

    refactor: list book header should only be shown while listing books

--- src/App.js
+++ src/App.js
@@ -5,52 +5,50 @@ import { Route, Link } from "react-router-dom";
 import SearchBooks from "./searchBooks.js"
 import BookShelf from "./BookShelf.js"
 class BooksApp extends React.Component {
-	state = {
-		books: [],
-		readBooks: [],
-		currentlyReadingBooks: [],
-		wantToReadBooks: []
-	}
-	componentDidMount() {
-		BooksAPI.getAll()
-			.then((booksFromAPI) => {
-				this.setState((prevState) => ({
-					books: booksFromAPI,
-					readBooks: booksFromAPI.filter((book) => (book.shelf === 'read')),
-					currentlyReadingBooks: booksFromAPI.filter((book) => (book.shelf === 'currentlyReading')),
-					wantToReadBooks: booksFromAPI.filter((book) => (book.shelf === 'wantToRead')),
-				}))
+		state = {
+			books: [],
+			readBooks: [],
+			currentlyReadingBooks: [],
+			wantToReadBooks: []
+		}
+		componentDidMount() {
+			BooksAPI.getAll()
+				.then((booksFromAPI) => {
+					this.setState((prevState) => ({
+						books: booksFromAPI,
+						readBooks: booksFromAPI.filter((book) => (book.shelf === 'read')),
+						currentlyReadingBooks: booksFromAPI.filter((book) => (book.shelf === 'currentlyReading')),
+						wantToReadBooks: booksFromAPI.filter((book) => (book.shelf === 'wantToRead')),
+					}))
+				});
+		}
+		refreshBookShelves = (response) => {
+			const changedBooks = this.state.books.map((book) => {
+				if (response.currentlyReading.includes(book.id)) {
+					book.shelf = "currentlyReading";
+				} else if (response.read.includes(book.id)) {
+					book.shelf = "read";
+				} else if (response.wantToRead.includes(book.id)) {
+					book.shelf = "wantToRead";
+				}
+				return book;
 			});
-	}
-	refreshBookShelves = (response) => {
-		const changedBooks = this.state.books.map((book) => {
-			if (response.currentlyReading.includes(book.id)) {
-				book.shelf = "currentlyReading";
-			} else if (response.read.includes(book.id)) {
-				book.shelf = "read";
-			} else if (response.wantToRead.includes(book.id)) {
-				book.shelf = "wantToRead";
-			}
-			return book;
-		});
-		this.setState(() => ({
-			books: changedBooks,
-			readBooks: changedBooks.filter((book) => (book.shelf === 'read')),
-			currentlyReadingBooks: changedBooks.filter((book) => (book.shelf === 'currentlyReading')),
-			wantToReadBooks: changedBooks.filter((book) => (book.shelf === 'wantToRead')),
-		}))
-	}
-	render() {
-		const { readBooks, currentlyReadingBooks, wantToReadBooks } = this.state;
-		return (<div className="app">
-        <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
+			this.setState(() => ({
+				books: changedBooks,
+				readBooks: changedBooks.filter((book) => (book.shelf === 'read')),
+				currentlyReadingBooks: changedBooks.filter((book) => (book.shelf === 'currentlyReading')),
+				wantToReadBooks: changedBooks.filter((book) => (book.shelf === 'wantToRead')),
+			}))
+		}
+		render() {
+			const { readBooks, currentlyReadingBooks, wantToReadBooks } = this.state;
+			return (<div className="app">
+        				<div className="list-books">
                 <Route exact path="/" render={()=>(
-                    <div>
+                    <div className="list-books">
+											<div className="list-books-title">
+												<h1>MyReads</h1>
+											</div>
                       <BookShelf refresh={(flag) => {
             							this.refreshBookShelves(flag)
             					}}
@@ -69,6 +67,10 @@ class BooksApp extends React.Component {
                         name="Read"
                         books={readBooks}>
                       </BookShelf>
+											<div className = "open-search">
+												<Link to='/search' className="add-contact">Add a book
+												</Link>
+											</div>
                   </div>
                 )}>
               </Route>
@@ -77,9 +79,7 @@ class BooksApp extends React.Component {
                   <SearchBooks books={this.state.books}/>
                 )}>
               </Route>
-            </div>
-        </div>
-      </div> < div className = "open-search" > <Link to='/search' className="add-contact">Add a book </Link> < /div> < /div > )
-	}
-}
-export default BooksApp
+        </div> < /div> )
+			}
+		}
+		export default BooksApp
