b6a79e945ca1a08109df42aa0ac219bb5a6d2c0f
Rashmi <rashmi.manandhar7@gmail.com>
2020-02-17 19:09:48 -0500

    chore: add linter for formatting

--- src/BookGrid.js
+++ src/BookGrid.js
@@ -1,22 +1,27 @@
 import React, { Component } from "react";
-import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from "./BooksAPI";
 import Book from "./Book.js";
 class BookGrid extends Component {
-	updateBookShelf = (book, changedShelf) => {
-		BooksAPI.update(book, changedShelf)
-			.then(response => {
-				if (this.props.refresh) {
-					this.props.refresh(response);
-				}
-			});
-	}
-	render() {
-		const { books } = this.props
-		return (<ol className="books-grid">
-					{books.map((book)=>(
-						<Book key={book.id} book={book} onShelfChange={this.updateBookShelf}/>
-					))}
-	        </ol>);
-	}
+  updateBookShelf = (book, changedShelf) => {
+    BooksAPI.update(book, changedShelf).then(response => {
+      if (this.props.refresh) {
+        this.props.refresh(response);
+      }
+    });
+  };
+  render() {
+    const { books } = this.props;
+    return (
+      <ol className="books-grid">
+        {books.map(book => (
+          <Book
+            key={book.id}
+            book={book}
+            onShelfChange={this.updateBookShelf}
+          />
+        ))}
+      </ol>
+    );
+  }
 }
 export default BookGrid;
