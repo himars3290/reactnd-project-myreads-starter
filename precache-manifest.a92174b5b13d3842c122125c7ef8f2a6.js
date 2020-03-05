99e0b2de012067a253904cacc84720b2d747733b
Rashmi <rashmi.manandhar7@gmail.com>
2020-03-05 12:34:04 -0500

    Updates

---
+++ precache-manifest.a92174b5b13d3842c122125c7ef8f2a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "1b7e853e01d73a6cc24d",
+    "url": "/myreads/static/js/runtime~main.1b7e853e.js"
+  },
+  {
+    "revision": "a3a9e16e0b5d2fe02d4f",
+    "url": "/myreads/static/js/main.a3a9e16e.chunk.js"
+  },
+  {
+    "revision": "daa725f361eb3067b087",
+    "url": "/myreads/static/js/1.daa725f3.chunk.js"
+  },
+  {
+    "revision": "a3a9e16e0b5d2fe02d4f",
+    "url": "/myreads/static/css/main.85cc2577.chunk.css"
+  },
+  {
+    "revision": "59b80969c1611ba6a19f9ded97fd0dd5",
+    "url": "/myreads/index.html"
+  }
+];
